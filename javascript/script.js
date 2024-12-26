9a0b99afe7545ffa75dbdf26dc7e69ff648d1597
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-26 12:16:32 +0100

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,20 @@
 const burger=document.querySelector(".bars");
 const navigation=document.querySelector("header ul")
+const rem=document.querySelector(".remov")
 
 burger.addEventListener("click", ()=>{
     navigation.classList.add("active")
+    burger.style.display='none'
+rem.style.display='block'
+});
+
+rem.addEventListener("click", ()=>{
+    navigation.classList.remove("active") 
+      burger.style.display='block'
+rem.style.display='none'
+});
+window.addEventListener("scroll", ()=>{
+    navigation.classList.remove("active") 
+      burger.style.display='block'
+rem.style.display='none'  
 })
