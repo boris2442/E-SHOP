dd0eb4b31817358fe060cc35e384b5191e21b29e
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-06 20:57:10 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,36 @@
-const burger=document.querySelector(".bars");
-const navigation=document.querySelector("header ul")
-const rem=document.querySelector(".remov")
-
-burger.addEventListener("click", ()=>{
-    navigation.classList.add("active")
-    burger.style.display='none'
-rem.style.display='block'
+const burger = document.querySelector(".bars");
+const navigation = document.querySelector("header ul");
+const rem = document.querySelector(".remov");
+const section=document.querySelectorAll("section");
+const linkNav=document.querySelectorAll("header ul li a")
+burger.addEventListener("click", () => {
+  navigation.classList.add("active");
+  burger.style.display = "none";
+  rem.style.display = "block";
 });
 
-rem.addEventListener("click", ()=>{
-    navigation.classList.remove("active") 
-      burger.style.display='block'
-rem.style.display='none'
+rem.addEventListener("click", () => {
+  navigation.classList.remove("active");
+  burger.style.display = "block";
+  rem.style.display = "none";
 });
-window.addEventListener("scroll", ()=>{
-    navigation.classList.remove("active") 
-      burger.style.display='block'
-rem.style.display='none'  
-})
-
-
+window.addEventListener("scroll", () => {
+  navigation.classList.remove("active");
+  burger.style.display = "block";
+  rem.style.display = "none";
+});
 
 // code reservé a la librairie scrollReveal
 
-const sr=ScrollReveal({
-    duration:1500,
-    origin:"top",
-    distance:"30px",
-    reset:true
-})
-sr.reveal(".droits, .footerscroll, .temoignage, .prods, .accueil, .news, .avis", {interval:200})
+const sr = ScrollReveal({
+  duration: 1500,
+  origin: "top",
+  distance: "30px",
+  reset: true,
+});
+sr.reveal(
+  ".droits, .footerscroll, .temoignage, .prods, .accueil, .news, .avis",
+  { interval: 200 }
+);
 
 
