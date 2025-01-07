d9b0dc139077b2c15dd84350af2dec98fe276e5a
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-07 08:29:03 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,9 @@ const burger = document.querySelector(".bars");
 const navigation = document.querySelector("header ul");
 const rem = document.querySelector(".remov");
 const sections=document.querySelectorAll("section");
-const linkNav=document.querySelectorAll("header ul li a")
+const link=document.querySelectorAll("header ul li a")
+// const home=document.querySelector(".home2");
+
 burger.addEventListener("click", () => {
   navigation.classList.add("active");
   burger.style.display = "none";
@@ -33,13 +35,34 @@ sr.reveal(
   { interval: 200 }
 );
 
-const scrooActive=()=>{
-sections.forEach((section)=>{
-    let height=section.offsetHeight;
-    let top=section.offsetTop;
-    let scrolls=windows.scrollY;
-    if(scrolls>=top - 400 && scrolls<top + height){
+// const scrollActive=()=>{
+// sections.forEach((section=>{
+//     let height=section.offsetHeight;
+//     let top=section.offsetTop;
+//     let scrolls=window.scrollY;
+//     let id=section.getAttribute("id");
+//     if(scrolls>=top - 400 && scrolls<top + height){
+//         console.log("hey")
+//         // linkNav.classList.remove("home2")
+//     }
+// })
+// }
 
-    }
-})
+const scrollActive= ()=>{
+    sections.forEach(section=>{
+        let height=section.offsetHeight;
+        let top=section.offsetTop;
+        let scrolls=window.scrollY; 
+       let id=section.getAttribute("id");
+       if(scrolls>=top-200 && scrolls<top + height){
+        link.forEach(links => {
+            links.classList.remove("home2")
+        });
+        console.log('alertt')
+       }
+       let recuperationId=document.querySelector(`header ul li a[href*=${id}]`)
+       recuperationId.classList.add("home2")
+    })
 }
+
+window.addEventListener("scroll", scrollActive)
